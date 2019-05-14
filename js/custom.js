9103c47a8b1d8ee8bf33143fbc6bd10dc5b2d769
uyouthe <melvoloskov@ya.ru>
2019-05-14 23:07:40 +0300

    logo and stuff

--- js/custom.js
+++ js/custom.js
@@ -4,10 +4,18 @@ const keys = Object.keys(dict)
 const wrap = (arr, template) => arr.map(template).join('')
 const goButton = document.getElementById('go')
 const resultsList = document.getElementById('results')
+const resultsBlock = document.getElementById('results-block')
 const chart = document.getElementById('chart')
 const skillInput = document.getElementById('skill')
 const sortObjDesc = obj => (a, b) => obj[b] - obj[a]
 const skillsDatalist = document.getElementById('skills')
+const whereToPrintOriginalSkill = document.getElementById('search-query')
+
+const scrollToResults = () => $(document.documentElement).animate({
+    scrollTop: $(resultsBlock).offset().top
+}, 300)
+
+const showResultsBlock = () => resultsBlock.removeAttribute('hidden')
 
 skillsDatalist.innerHTML = wrap(keys, key => `
   <option value="${key}">
@@ -19,12 +27,26 @@ goButton.addEventListener('click', () => {
   const value = skillInput.value
   const skill = dict[value]
 
-  if (skill) {
+  if (skill && value) {
     const similars = Object.keys(skill).sort(sortObjDesc(skill))
 
     resultsList.innerHTML = wrap(similars, similar => `
-      <li>${similar}: ${skill[similar]}</li>
+      <div class="frame-97 ae-3 padding-bottom-3 margin-top-2">
+        <h2 class="small align-left cropBottom inline-block padding-top-1">
+          <span class="light">
+            ${similar}
+          </span>
+          <span class="regular stat">
+            ${skill[similar]}
+          </span>
+        </h2>
+      </div>
     `)
+
+    whereToPrintOriginalSkill.innerHTML = value
+
+    showResultsBlock()
+    scrollToResults()
 
     // new Chart(chart, {
     //   type: 'pie',
